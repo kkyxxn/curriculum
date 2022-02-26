cdcf50e9138a9a70bef53bb36b88d9e207071b02
kkyxxn <96809488+kkyxxn@users.noreply.github.com>
2022-02-27 03:21:09 +0900

    Add files via upload

--- m5_데이터베이스구축및활용/ddl.sql
+++ m5_데이터베이스구축및활용/ddl.sql
@@ -1,4 +1,4 @@
-dssd--[테이블 생성 규칙]
+--[테이블 생성 규칙]
 
 --테이블명은 객체를 의미할 수 있는 적절한 이름을 사용한다. 가능한 단수형을 권고한다.--
 --테이블명은 다른 테이블의 이름과 중복되지 않아야 한다.--
@@ -39,12 +39,14 @@ PHONE    VARCHAR2(13),
 REGDATE  DATE
 );
 
---일부 속성만 입력
+--일부 속성만 입력도 가능함
 INSERT INTO MEMBER(ID, PWD, NAME)VALUES('200901','111','KEVIN'); 
 
 SELECT * FROM MEMBER;
+DESC MEMBER;
+
 --전체 모두 입력
-INSERT INTO MEMBER VALUES('200902','112','JAMES','M',29,'01-JAN-99','010-1234-2345','1994/05/02');
+INSERT INTO MEMBER VALUES('200902','112','JAMES','M',29,'01-JAN-99','010-1234-2345','1994/05/02','치킨');
 
 SELECT * FROM MEMBER;
 
@@ -52,11 +54,13 @@ ALTER TABLE MEMBER ADD TEXT NCLOB;
 
 INSERT INTO MEMBER(ID, PWD,TEXT) VALUES('200903','112','정치는 국민을 위해 존재한다');
 
---기존 테이블을 이용하여 새로운 테이블을 생성
+--기존 테이블을 이용하여 새로운 테이블을 생성 -> 그대로 복사해줌
 create table member1 as select * from member;
-SELECT * FROM MEMBER1;
+SELECT * FROM member1;
 DESC member1;
+
 CREATE TABLE member2 AS SELECT * FROM member1 WHERE 1=0;
+SELECT * FROM member2;
 DESC member2;
 
 --테이블의 모든 row 삭제
@@ -64,37 +68,54 @@ TRUNCATE TABLE member1;
 
 --테이블 속성 및 타입 조회
 DESC MEMBER;
+
 --테이블 리스트 조회
 SELECT * FROM TABS;
 
 --ALTER
 DESC MEMBER1;
---수정
+--데이터 타입 수정
 ALTER TABLE MEMBER1 MODIFY(ID VARCHAR2(50), NAME NVARCHAR2(50));
---변경
+--컬럼명 변경
 ALTER TABLE MEMBER1 RENAME COLUMN BIRTHDAY TO BD;
---삭제
+--컬럼 삭제
 ALTER TABLE MEMBER1 DROP COLUMN AGE;
---추가
+--컬럼 추가
 ALTER TABLE MEMBER1 ADD AGE NUMBER;
-
+--제약조건 설정
 ALTER TABLE MEMBER1 ADD CONSTRAINT MEMBER1_PK PRIMARY KEY (ID);
 
 --[과제] MEMBER2 테이블을 생성한 후 수정, 변경, 삭제, 추가 작업을 수행하세요.(학생 이력 table, 속성은 5개 이상)
 
-테이블명 : PLAYER
-테이블 설명 : K-리그 선수들의 정보를 가지고 있는 테이블
-칼럼명 : PLAYER_ID (선수ID) 문자 고정 자릿수 7자리,
-PLAYER_NAME (선수명) 문자 가변 자릿수 20자리,
-TEAM_ID (팀ID) 문자 고정 자릿수 3자리,
-JOIN_YYYY (입단년도) 문자 고정 자릿수 4자리,
-POSITION (포지션) 문자 가변 자릿수 10자리,
-BACK_NO (등번호) 숫자 2자리,
-NATION (국적) 문자 가변 자릿수 20자리,
-BIRTH_DATE (생년월일) 날짜,
-제약조건 : 기본키(PRIMARY KEY) :  PLAYER_ID
-(제약조건명은 PLAYER_PK)
-값이 반드시 존재 (NOT NULL) : PLAYER_NAME, TEAM_ID
-21
+--Q. 아래 정보를 기반으로 PLAYER 테이블을 생성하세요.
+--테이블명 : PLAYER
+--테이블 설명 : K-리그 선수들의 정보를 가지고 있는 테이블
+--칼럼명 : 
+--PLAYER_ID (선수ID) 문자 고정 자릿수 7자리,
+--PLAYER_NAME (선수명) 문자 가변 자릿수 20자리,
+--TEAM_ID (팀ID) 문자 고정 자릿수 3자리,
+--JOIN_YYYY (입단년도) 문자 고정 자릿수 4자리,
+--POSITION (포지션) 문자 가변 자릿수 10자리,
+--BACK_NO (등번호) 숫자 2자리,
+--NATION (국적) 문자 가변 자릿수 20자리,
+--BIRTH_DATE (생년월일) 날짜,
+--제약조건 : 기본키(PRIMARY KEY) :  PLAYER_ID
+--(제약조건명은 PLAYER_PK)
+--값이 반드시 존재 (NOT NULL) : PLAYER_NAME, TEAM_ID
+
+CREATE TABLE PLAYER(
+PLAYER_ID NCHAR(7),
+PLAYER_NAME VARCHAR(20) NOT NULL,
+TEAM_ID NCHAR(3) NOT NULL,
+JOIN_YYYY NCHAR(4),
+POSITION VARCHAR(10),
+BACK_NO NUMBER(2),
+NATION VARCHAR(20),
+BIRTH_DATE DATE,
+CONSTRAINT PLAYER_PK PRIMARY KEY(PLAYER_ID)
+);
+
+DESC PLAYER;
+DROP TABLE PLAYER;
 
 
